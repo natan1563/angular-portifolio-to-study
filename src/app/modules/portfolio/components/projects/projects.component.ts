96600407f71d19126b34bab593fab8a5772f757e
natan1563 <natromafe@gmail.com>
2024-02-29 21:54:44 -0300

    style from homepage

--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -1,16 +1,22 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { IProjects } from '../../interface/IProjects.interface';
+
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { EDialogPanelClass } from '../../enum/EDialogPanelClass.enum';
+import { DialogProjectsComponent } from '../dialog/dialog-projects/dialog-projects.component';
 
 const baseImagesPath = 'assets/img/projects'
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [MatDialogModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+  #dialog = inject(MatDialog);
+
   public arrayProjects = signal<IProjects[]>([
     {
       src: `${baseImagesPath}/vfull.png`,
@@ -25,6 +31,83 @@ export class ProjectsComponent {
           href: '#'
         }
       ]
-    }
+    },
+    {
+      src: `${baseImagesPath}/vfull.png`,
+      alt: "Projeto vida fullstack",
+      title: "Vida full Stack",
+      width: "100px",
+      height: "51px",
+      description: "<p>Explore um projeto muito legal.</p>",
+      links: [
+        {
+          name: 'Conheça o Blog',
+          href: '#'
+        }
+      ]
+    },
+    {
+      src: `${baseImagesPath}/vfull.png`,
+      alt: "Projeto vida fullstack",
+      title: "Vida full Stack",
+      width: "100px",
+      height: "51px",
+      description: "<p>Explore um projeto muito legal.</p>",
+      links: [
+        {
+          name: 'Conheça o Blog',
+          href: '#'
+        }
+      ]
+    },
+    {
+      src: `${baseImagesPath}/vfull.png`,
+      alt: "Projeto vida fullstack",
+      title: "Vida full Stack",
+      width: "100px",
+      height: "51px",
+      description: "<p>Explore um projeto muito legal.</p>",
+      links: [
+        {
+          name: 'Conheça o Blog',
+          href: '#'
+        }
+      ]
+    },
+    {
+      src: `${baseImagesPath}/vfull.png`,
+      alt: "Projeto vida fullstack",
+      title: "Vida full Stack",
+      width: "100px",
+      height: "51px",
+      description: "<p>Explore um projeto muito legal.</p>",
+      links: [
+        {
+          name: 'Conheça o Blog',
+          href: '#'
+        }
+      ]
+    },
+    {
+      src: `${baseImagesPath}/vfull.png`,
+      alt: "Projeto vida fullstack",
+      title: "Vida full Stack",
+      width: "100px",
+      height: "51px",
+      description: "<p>Explore um projeto muito legal.</p>",
+      links: [
+        {
+          name: 'Conheça o Blog',
+          href: '#'
+        }
+      ]
+    },
   ])
+
+  public openDialog(data: IProjects) {
+    this.#dialog.open(DialogProjectsComponent, {
+      data,
+      panelClass: EDialogPanelClass.PROJECTS
+    })
+  }
 }
